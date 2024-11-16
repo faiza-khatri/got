f44d2f8a4330f4a37c949fc7148a7ff55b4bd86d
Faiza Salman Khatri <fs09192@st.habib.edu.pk>
2024-11-16 22:01:12 +0500

    testing branch functionalities

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include <SFML/Graphics.hpp>
+#include "characterSelectWindow.h"
+
+int main() {
+    //// Create an instance of CharacterSelectWindow
+    //CharacterSelectWindow characterSelectWindow;
+
+    //// Set the window size and title
+    //characterSelectWindow.create(sf::VideoMode(800, 600), "Character Select");
+
+    //// Initialize the character list with portraits
+    //characterSelectWindow.initialize();
+
+    //// Main loop
+    //while (characterSelectWindow.isOpen()) {
+    //    sf::Event event;
+    //    while (characterSelectWindow.pollEvent(event)) {
+    //        if (event.type == sf::Event::Closed) {
+    //            characterSelectWindow.close();
+    //        }
+    //    }
+
+    //    // Clear and draw the characters in the window
+    //    characterSelectWindow.updateGraphics();
+
+    //}
+
+    return 0;
+}
